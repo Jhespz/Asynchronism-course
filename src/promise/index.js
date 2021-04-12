10e959f5359edf67646dff2d2cd39f0492c96c75
Jhespz <jstiven.ez@gmail.com>
2021-04-12 10:22:39 -0500

    Se añaden más ejemplos de promesas en el index.js y se utiliza Promise.all para múltiples promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -1,5 +1,5 @@
 
-const somethingWillhappen = () => {
+const somethingWillhappen1 = () => {
     return new Promise((resolve, reject) => {
         if (true) {
             resolve('Hey!');
@@ -9,6 +9,41 @@ const somethingWillhappen = () => {
     })
 }
 
-somethingWillhappen()
+somethingWillhappen1()
     .then(response => console.log(response))
     .catch(err => console.log(err))
+
+
+const somethingWillhappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000)
+        } else {
+            const error = new Error('Whoops"')
+                reject(error)
+        }
+    })
+}
+
+somethingWillhappen2()
+    .then(response => console.log(response))
+    .catch(err => console.log(err))
+
+//Multiples promesas
+
+Promise.all([somethingWillhappen1(), somethingWillhappen2()])
+    .then(response => {
+        console.log('Array of results', response)
+    })
+
+    .catch(err => {
+        console.error(err)
+    })
+
+/*
+Hey!
+True
+Array of results [ 'Hey!', 'True' ]
+*/
