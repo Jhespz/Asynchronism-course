ecb3bb0015c5dd4647f7e41ed453349e8e44c5dd
Jhespz <jstiven.ez@gmail.com>
2021-04-11 22:51:13 -0500

    Se comentan ejemplos, se crea el archivo donde estará el reto y se instala el paquete xmlhttprequest de npm

--- src/callback/index.js
+++ src/callback/index.js
@@ -1,14 +1,18 @@
-function sum(num1, num2) {
-    return num1 + num2;
-}
+//---------------------Ejemplo 1--------------------
 
-function calc(num1, num2, callback) {
-    return callback(num1, num2);
-}
+// function sum(num1, num2) {
+//     return num1 + num2;
+// }
 
-console.log(calc(6,2,sum));
+// function calc(num1, num2, callback) {
+//     return callback(num1, num2);
+// }
 
-//--------------------*************************------------------
+// console.log(calc(6,2,sum));
+
+
+//---------------------Ejemplo 2--------------------
+
 
 function date(callback) {
     console.log('primer console log', new Date);
